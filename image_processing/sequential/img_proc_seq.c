bde8036f7cc5ae1b2f5dd94d474c2e648b8167f6
JoeyValentine <rlaalsdn0506@naver.com>
2019-11-04 07:28:55 -0800

    Create image_processing/sequential/img_proc_seq.c

---
+++ image_processing/sequential/img_proc_seq.c
@@ -0,0 +1,19 @@
+#include "img_proc_seq.h"
+
+
+int flip_seq(const Image *input_img_ptr, Image *output_img_ptr)
+{
+
+}
+
+
+int rgb2gray_seq(const Image *input_img_ptr, Image *output_img_ptr)
+{
+
+}
+
+
+int smoothing_seq(const Image *input_img_ptr, Image *output_img_ptr)
+{
+
+}
