b0ebe84ccbcc115be22151e8ea577c8acfa438e3
JoeyValentine <rlaalsdn0506@naver.com>
2019-11-04 11:59:53 -0800

    Fix image.c

--- image_processing/sequential/image.c
+++ image_processing/sequential/image.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "exception.h"
 #include "image.h"
 
-int read_ppm_img(const char *file_name, Image *img_ptr)
+int read_ppm_img(const char *file_name, Image **img_ptr)
 {
 	FILE *fp;
-	char buff[256] ={0,};
-	unsigned int n_row, n_col, max_color_val;
+	char buff[256] ={0,}, c;
+	unsigned int n_row, n_col, max_color_val, bytes_read;
 	
 	fp = fopen(file_name, "rb");
 
 	if (fp == NULL)
 		return ERR_FILE;
 	
-	if (fgets(buff, sizeof(buff), fp) != EOF)
+	if (fgets(buff, sizeof(buff), fp) == EOF)
 		return ERR_FORMAT;
 
 	if (buff[0] != 'P' || buff[1] != '6')
 		return ERR_FORMAT;
 
-	img_ptr = malloc(sizeof(Image));
+	*img_ptr = malloc(sizeof(Image));
 	
-	if (img_ptr == NULL)
+	if (*img_ptr == NULL)
 		return ERR_MEM;
 
-	char c;
-    	
 	c = getc(fp);
     	while (c == '#') 
 	{
@@ -34,8 +33,11 @@ int read_ppm_img(const char *file_name, Image *img_ptr)
     	}
     	ungetc(c, fp);
 
-	if (fscanf(fp, "%u %u", img_ptr->n_col, img_ptr->n_row) != 2)
+	if (fscanf(fp, "%u %u", &n_col, &n_row) != 2)
 		return ERR_FORMAT;
+	
+	(*img_ptr)->n_col = n_col;
+	(*img_ptr)->n_row = n_row;
 
     	if (fscanf(fp, "%d", &max_color_val) != 1)
 		return ERR_FORMAT;
@@ -45,15 +47,16 @@ int read_ppm_img(const char *file_name, Image *img_ptr)
 
     	while (fgetc(fp) != '\n');
 
-	img_ptr->arr = malloc(n_row * n_col * sizeof(Pixel));
+	(*img_ptr)->arr = malloc(n_row * n_col * sizeof(Pixel));
 	
-	if (img_ptr->arr == NULL)
+	if ((*img_ptr)->arr == NULL)
 		return ERR_MEM;
 	
-    	if (fread(img_ptr->arr, BYTES_PER_PIXEL * img_ptr->n_col, 
-				img_ptr->n_row, fp) != img_ptr->n_row)
+    	bytes_read = fread((*img_ptr)->arr, sizeof(unsigned char), BYTES_PER_PIXEL * n_col * n_row, fp);
+	
+	if (bytes_read != BYTES_PER_PIXEL * n_col * n_row)
 		return ERR_FILE;
-
+	
 	if (fclose(fp) == EOF)
 		return ERR_FILE;
 
@@ -65,6 +68,9 @@ int write_ppm_img(const char *file_name, const Image *img_ptr)
 	FILE *fp;
 	unsigned int n_row, n_col, bytes_written;
 	
+	n_col = img_ptr->n_col;
+	n_row = img_ptr->n_row;
+	
 	fp = fopen(file_name, "wb");
 	
 	if (fp == NULL)
@@ -73,8 +79,8 @@ int write_ppm_img(const char *file_name, const Image *img_ptr)
 	if (fprintf(fp, "P6\n%u %u\n%u\n", n_col, n_row, MAX_COLOR_VAL) == EOF)
 		return ERR_FILE;
 	
-	bytes_written = fwrite(img_ptr->arr, BYTES_PER_PIXEL * n_col * n_row, fp);
-
+	bytes_written = fwrite(img_ptr->arr, sizeof(unsigned char), BYTES_PER_PIXEL * n_col * n_row, fp);
+	
 	if (bytes_written != BYTES_PER_PIXEL * n_col * n_row)
 		return ERR_FILE;
 
@@ -86,6 +92,6 @@ int write_ppm_img(const char *file_name, const Image *img_ptr)
 
 void free_img(Image *img_ptr)
 {
-	free(img_ptr->arr)
+	free(img_ptr->arr);
 	free(img_ptr);
 }
