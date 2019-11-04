256cbdc6dad7cc13eb88492f72dbfd50a63f8582
JoeyValentine <rlaalsdn0506@naver.com>
2019-11-04 07:33:43 -0800

    Create image_processing/sequential/image.h

---
+++ image_processing/sequential/image.h
@@ -0,0 +1,22 @@
+#ifndef __IMAGE__
+#define __IMAGE__
+
+typedef struct _Image
+{
+	unsigned int n_row;
+	unsigned int n_col;
+	PixelPtr *arr;
+} Image;
+
+typedef struct _Pixel
+{
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+} Pixel;
+
+int read_ppm_img(const char *file_name, Image *img_ptr);
+int write_ppm_img(const char *file_name, const Image *img_ptr);
+void free_img(Image *img_ptr);
+
+#endif
