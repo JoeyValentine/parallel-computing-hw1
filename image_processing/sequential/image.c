322f1f6bdddc3f4c13f7febeaafdbf48c0641ce2
JoeyValentine <rlaalsdn0506@naver.com>
2019-11-04 07:33:24 -0800

    Create image_processing/sequential/image.c

---
+++ image_processing/sequential/image.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int read_ppm_img(const char *file_name, Image *img_ptr)
+{
+	
+}
+
+
+int write_ppm_img(const char *file_name, const Image *img_ptr)
+{
+
+}
+
+
+void free_img(Image *img_ptr)
+{
+
+}
