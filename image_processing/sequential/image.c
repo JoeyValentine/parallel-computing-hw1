d30e8502a81ebc529d3840e1057859a05c43d4df
JoeyValentine <rlaalsdn0506@naver.com>
2019-11-04 10:18:29 -0800

    Implement free_img()

--- image_processing/sequential/image.c
+++ image_processing/sequential/image.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "image.h"
 
 int read_ppm_img(const char *file_name, Image *img_ptr)
 {
-	
-}
 
+}
 
 int write_ppm_img(const char *file_name, const Image *img_ptr)
 {
 
 }
 
-
 void free_img(Image *img_ptr)
 {
-
+	free(img_ptr->arr)
+	free(img_ptr);
 }
