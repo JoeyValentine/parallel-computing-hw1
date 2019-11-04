927c6f7899eb455e33d43d3aa88f269d591b47f6
JoeyValentine <rlaalsdn0506@naver.com>
2019-11-04 10:21:11 -0800

    Implement write_ppm_img()

--- image_processing/sequential/image.c
+++ image_processing/sequential/image.c
@@ -9,7 +9,26 @@ int read_ppm_img(const char *file_name, Image *img_ptr)
 
 int write_ppm_img(const char *file_name, const Image *img_ptr)
 {
+	FILE *fp;
+	unsigned int n_row, n_col, bytes_written;
+	
+	fp = fopen(file_name, "wb");
+	
+	if (fp == NULL)
+		return ERR_FILE;
 
+	if (fprintf(fp, "P6\n%u %u\n%u\n", n_col, n_row, MAX_COLOR_VAL) == EOF)
+		return ERR_FILE;
+	
+	bytes_written = fwrite(img_ptr->arr, BYTES_PER_PIXEL * n_col * n_row, fp);
+
+	if (bytes_written != BYTES_PER_PIXEL * n_col * n_row)
+		return ERR_FILE;
+
+	if (fclose(fp) == EOF)
+		return ERR_FILE;
+
+	return 0;
 }
 
 void free_img(Image *img_ptr)
