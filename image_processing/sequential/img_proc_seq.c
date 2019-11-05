825c466b7b4313631438919bb3e0da0b912b7b93
JoeyValentine <rlaalsdn0506@naver.com>
2019-11-05 03:14:27 -0800

    Convert output ptr from single ptr to double ptr

--- image_processing/sequential/img_proc_seq.c
+++ image_processing/sequential/img_proc_seq.c
@@ -1,19 +1,18 @@
 #include "img_proc_seq.h"
 
-
-int flip_seq(const Image *input_img_ptr, Image *output_img_ptr)
+int flip_seq(const Image *input_img_ptr, Image **output_img_ptr)
 {
 
 }
 
 
-int rgb2gray_seq(const Image *input_img_ptr, Image *output_img_ptr)
+int rgb2gray_seq(const Image *input_img_ptr, Image **output_img_ptr)
 {
 
 }
 
 
-int smoothing_seq(const Image *input_img_ptr, Image *output_img_ptr)
+int smoothing_seq(const Image *input_img_ptr, Image **output_img_ptr)
 {
 
 }
