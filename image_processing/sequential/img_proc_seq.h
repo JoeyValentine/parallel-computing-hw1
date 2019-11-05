2331d88dc7c3f91359cb5e3e9a45e2b50411ad22
JoeyValentine <rlaalsdn0506@naver.com>
2019-11-05 05:08:15 -0800

    Move asterisk in gen_processed_img()

--- image_processing/sequential/img_proc_seq.h
+++ image_processing/sequential/img_proc_seq.h
@@ -10,6 +10,6 @@ extern const char* function_names[N_FUNC];
 void flip_seq(const Image *input_img_ptr, Image *output_img_ptr);
 void rgb2gray_seq(const Image *input_img_ptr, Image *output_img_ptr);
 void smoothing_seq(const Image *input_img_ptr, Image *output_img_ptr);
-int gen_processed_img(const Image *input_img_ptr, const char* func_name)
+int gen_processed_img(const Image *input_img_ptr, const char *func_name)
 
 #endif
