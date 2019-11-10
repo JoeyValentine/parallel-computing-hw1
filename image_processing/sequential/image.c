b137516bee90c842396c862d74db926af512a7d8
JoeyValentine <rlaalsdn0506@naver.com>
2019-11-09 16:56:23 -0800

    Add gen_padded_img()

--- image_processing/sequential/image.c
+++ image_processing/sequential/image.c
@@ -95,7 +95,7 @@ int write_ppm_img(const char *file_name, const Image *img_ptr)
 
 int gen_same_shape_img(const Image *input_img_ptr, Image **output_img_ptr)
 {
-	unsigned int  n_row, n_col;
+	unsigned int n_row, n_col;
 	
 	n_row = input_img_ptr->n_row;
 	n_col = input_img_ptr->n_col;
@@ -116,8 +116,57 @@ int gen_same_shape_img(const Image *input_img_ptr, Image **output_img_ptr)
 	return 0;
 }
 
+int gen_padded_img(const Image *input_img_ptr, Image **output_img_ptr)
+{
+	unsigned int n_row, n_col, input_base_idx, output_base_idx;
+	
+	n_row = input_img_ptr->n_row;
+	n_col = input_img_ptr->n_col;
+	
+	*output_img_ptr = malloc(sizeof(Image));
+	
+	if (*output_img_ptr == NULL)
+		return ERR_MEM;
+	
+	(*output_img_ptr)->n_col = n_col + 2;
+	(*output_img_ptr)->n_row = n_row + 2;
+
+	(*output_img_ptr)->arr = malloc((n_row + 2) * (n_col + 2) * sizeof(Pixel));
+	
+	if ((*output_img_ptr)->arr == NULL)
+		return ERR_MEM;
+
+	input_base_idx = n_col * (n_row - 1);
+	output_base_idx = (n_col + 2) * (n_row + 2 - 1);
+	
+	(*output_img_ptr)->arr[0] = input_img_ptr->arr[0];
+	(*output_img_ptr)->arr[n_col + 1] = input_img_ptr->arr[n_col - 1];
+	(*output_img_ptr)->arr[output_base_idx] = input_img_ptr->arr[input_base_idx];
+	(*output_img_ptr)->arr[output_base_idx + n_col + 1] = input_img_ptr->arr[input_base_idx + n_col - 1];
+	
+	for (int i = 0; i < n_col; ++i)
+	{
+		(*output_img_ptr)->arr[i + 1] = input_img_ptr->arr[i];
+		(*output_img_ptr)->arr[output_base_idx + i + 1] = input_img_ptr->arr[input_base_idx + i];
+	}
+
+/*
+	for (int i = 0; i < n_row; ++i)
+		for (int j = 0; j < n_col; ++j)
+			(*output_img_ptr)->arr[(n_col + 2) * (i + 1) + j + 1] = input_img_ptr->arr[n_col * i + j];
+
+	for (int i = 0; i < n_row; ++i)
+	{
+		(*output_img_ptr)->arr[(n_col + 2) * (i + 1)] = input_img_ptr->arr[n_col * i];
+		(*output_img_ptr)->arr[(n_col + 2) * (i + 1) + n_col + 1] = input_img_ptr->arr[n_col * i + n_col - 1];
+	}	
+*/
+	return 0;
+}
+
 void free_img(Image *img_ptr)
 {
 	free(img_ptr->arr);
 	free(img_ptr);
 }
+
