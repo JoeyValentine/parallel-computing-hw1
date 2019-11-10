0a93f3c4e0c3ef6f53f01d5705e6314265e8b93f
JoeyValentine <rlaalsdn0506@naver.com>
2019-11-09 16:56:45 -0800

    Add gen_padded_img()

--- image_processing/sequential/image.h
+++ image_processing/sequential/image.h
@@ -21,6 +21,7 @@ extern const unsigned int MAX_COLOR_VAL;
 int read_ppm_img(const char *file_name, Image **img_ptr);
 int write_ppm_img(const char *file_name, const Image *img_ptr);
 int gen_same_shape_img(const Image *input_img_ptr, Image **output_img_ptr);
+int gen_padded_img(const Image *input_img_ptr, Image **output_img_ptr);
 void free_img(Image *img_ptr);
 
 #endif
