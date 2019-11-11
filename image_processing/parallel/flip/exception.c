2ec2cdc3d68d2f580c5fe176f477d1adde7c95e1
JoeyValentine <rlaalsdn0506@naver.com>
2019-11-10 22:55:19 -0800

    Create files

---
+++ image_processing/parallel/flip/exception.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "exception.h"
+
+void print_exception_type(int exit_status, const char *file_name)
+{
+	switch (exit_status)
+	{
+		case ERR_MEM:
+			printf("ERROR: Memory allocation failed!\n");
+			break;
+		case ERR_FORMAT:
+			printf("ERROR: %s is not a valid ppm file!\n", file_name);
+			break;
+		case ERR_TRUNC:
+			printf("ERROR: %s is truncated", file_name);
+			break;
+		case ERR_FILE:
+			printf("ERROR: Error opening or closing %s", file_name);
+			break;
+		case ERR_USE:
+			printf("Usage: %s <input-ppm-file> <flipped-ppm-file>\n", file_name);
+			break;
+	}
+}
---
+++ image_processing/parallel/flip/image.h
@@ -0,0 +1,27 @@
+#ifndef __IMAGE__
+#define __IMAGE__
+
+typedef struct _Pixel
+{
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+} Pixel;
+
+typedef struct _Image
+{
+	unsigned int n_row;
+	unsigned int n_col;
+	Pixel *arr;
+} Image;
+
+extern const unsigned int BYTES_PER_PIXEL; 
+extern const unsigned int MAX_COLOR_VAL;
+
+int read_ppm_img(const char *file_name, Image **img_ptr);
+int write_ppm_img(const char *file_name, const Image *img_ptr);
+int gen_new_img(Image **output_img_ptr, unsigned int n_row, unsigned int n_col);
+int gen_padded_img(const Image *input_img_ptr, Image **output_img_ptr);
+void free_img(Image *img_ptr);
+
+#endif
