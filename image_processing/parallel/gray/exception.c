b73ae391c01aa0bb36985b58ac62da90c4d506aa
JoeyValentine <rlaalsdn0506@naver.com>
2019-11-10 23:17:49 -0800

    Modify exception.c

--- image_processing/parallel/gray/exception.c
+++ image_processing/parallel/gray/exception.c
@@ -12,13 +12,14 @@ void print_exception_type(int exit_status, const char *file_name)
 			printf("ERROR: %s is not a valid ppm file!\n", file_name);
 			break;
 		case ERR_TRUNC:
-			printf("ERROR: %s is truncated", file_name);
+			printf("ERROR: %s is truncated\n", file_name);
 			break;
 		case ERR_FILE:
-			printf("ERROR: Error opening or closing %s", file_name);
+			printf("ERROR: Error opening or closing %s\n", file_name);
 			break;
 		case ERR_USE:
-			printf("Usage: %s <input-ppm-file> <gray-scale-ppm-file>\n", file_name);
+			printf("Usage: mpirun -np <num processes> -hostfile "
+				"<hostfile> %s <input-ppm-file> <gray-scale-ppm-file>\n", file_name);
 			break;
 	}
 }
